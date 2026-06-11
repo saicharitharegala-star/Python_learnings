a10ddd2521a783e1f88e7153500f05b8d163870c
saicharitharegala-star <saicharith.aregala@gmail.com>
2026-06-11 14:36:26 +0530

    Add initial implementations for Calculator, Greeting, and Even or Odd programs

---
+++ Assignment 1/Calculator.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number :"))
+
+num2 = float(input("Enter second number :"))
+
+print(f"Addition : {num1 + num2}")
+print(f"Subtraction : {num1 - num2}")
+print(f"Multiplication : {num1 * num2}")
+print(f"Division : {num1 / num2}")
---
+++ Assignment 1/Greeting.py
@@ -0,0 +1,4 @@
+First_name = input("Enter your first name :")
+Last_name = input("Enter your last name :")
+print(f"Hello {First_name +' '+ Last_name}! welcome to the python program")
+
