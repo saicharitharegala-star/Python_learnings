96dcfe04161bcbb14b84aa229ab8b396320d58be
saicharitharegala-star <saicharith.aregala@gmail.com>
2026-06-11 15:19:53 +0530

    Remove .DS_Store and add .gitignore to improve performance

---
+++ Asignment 2/Even or odd.py
@@ -0,0 +1,10 @@
+number = int(input("Enter a number to check if its even or odd :"))
+
+if number % 2 == 0:
+    print(f"{number} is an even number!!")
+else:
+    print(f"{number} is an odd number!!")
+'''This program takes a number as input and checks if it is even or odd by using the modulus operator. 
+If the number is divisible by 2 with no remainder, 
+it is classified as even; otherwise, it is classified as odd.
+ The result is then printed to the user.'''
